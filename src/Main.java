f2a51c363ba2ef697179f74561c85bc788aa310f
KshishOff <krasavinmaxx@gmail.com>
2023-02-27 12:48:14 +0300

    gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 12000;
+        int price = 13020;
         int x = 20;
         int bonus = price / x;
         System.out.println("Ваши бонусные мили:" + bonus);
