607fd9d15c69036644621dbca6977ad7f948f8d8
KshishOff <krasavinmaxx@gmail.com>
2023-02-27 11:07:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 12000;
+        int x = 20;
+        int bonus = price / x;
+        System.out.println("Ваши бонусные мили:" + bonus);
+    }
+}
