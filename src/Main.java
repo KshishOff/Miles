fc807d2a531de5ee0891584850caaf8d2b612990
KshishOff <krasavinmaxx@gmail.com>
2023-02-28 11:18:38 +0300

    BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 13_020;
-        int bonus = service.calculate(price);;
+        int bonus = service.calculate(price);
         System.out.println("Ваши бонусные мили:" + bonus);
     }
 }
